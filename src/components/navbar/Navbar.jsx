9a2c64ef0a2a880dba9b017f8e9041708bfdb22c
cyber06917 <lalitkumarcyber069@gmail.com>
2023-01-07 18:32:19 +0530

    added a nav bar with css

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './navbar.css'
+import Logo from '../../assets/svg/logo.svg'
 
 function Navbar() {
   return (
-    <div>
-      <h1>Navbar</h1>
+    <div className='nav'>
+      <div className='nav__gpt-logo'>
+          <img src={Logo} alt="gpt-logo" />
+      </div>
+      <div className='nav__link'>
+          <a href="#Home">Home</a>
+          <a href="#Home">What is GPT?</a>
+          <a href="#Home">Open AI</a>
+          <a href="#Home">Case Studies</a>
+          <a href="#Home">Library</a>
+      </div>
     </div>
   )
 }
