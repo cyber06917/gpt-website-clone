72f9a15f196cf899f49422ccb00fa4deeeb6cd8a
Cyber <lalitkumarcyber069@gmail.com>
2023-01-09 00:42:34 +0530

    refracting a navbar code "incomplete navbar"

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,16 +4,26 @@ import Logo from '../../assets/svg/logo.svg'
 
 function Navbar() {
   return (
-    <div className='nav'>
-      <div className='nav__gpt-logo'>
+    <div className='gpt-navbar'>
+      <div className='gpt-navbar__link'>
+        <div className='gpt-navbar__link__logo'>
           <img src={Logo} alt="gpt-logo" />
+        </div>
+        <ul className='gpt-navbar__link__container'>
+          <li><a href="Home">Home</a></li>
+          <li><a href="What is GPT?">What is GPT?</a></li>
+          <li><a href="Open AI">Open AI</a></li>
+          <li><a href="Case Studies">Case Studies</a></li>
+          <li><a href="Library">Library</a></li>
+        </ul>
       </div>
-      <div className='nav__link'>
-          <a href="#Home">Home</a>
-          <a href="#Home">What is GPT?</a>
-          <a href="#Home">Open AI</a>
-          <a href="#Home">Case Studies</a>
-          <a href="#Home">Library</a>
+      <div className='gpt-navbar__button'>
+        <p>
+          sign in
+        </p>
+        <button type='button'>
+          Sign up
+        </button>
       </div>
     </div>
   )
