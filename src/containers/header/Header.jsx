e01e4083cddd93af411a64219eada08ce8812c81
cyber06917 <lalitkumarcyber069@gmail.com>
2023-01-09 17:56:41 +0530

    added a header portion with incomplete css styling

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import './header.css'
+import AI from '../../assets/images/ai.png'
+
 
 function Header() {
   return (
-    <div>
-      <h1>header</h1>
+    <div className='header'>
+      <div className='header__left'>
+        <h1>
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          Yet bed any for travellng assistance indulgence upleassing.
+          Not thoughts all exercise blessing. Indulgence way everthing joy
+          alteration boisterous the attachement. Party we years to order allow asked of.
+        </p>
+        <div className='header__left__get-started'>
+          <input type="text" />
+          <button>
+            get started
+          </button>
+        </div>
+        <div className='header__left__visitors'>
+          <img src="" alt="" />
+          <p></p>
+        </div>
+      </div>
+      <div className='header__ai-image'>
+          <img src={AI} alt="AI" />
+      </div>
     </div>
   )
 }
